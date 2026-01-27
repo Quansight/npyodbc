1c941ae438a51800a495d86cc9b4b51dbeb92a7f
pdmurray <peynmurray@gmail.com>
2026-01-27 13:43:06 -0800

    Remove debug stuff; add more documentation

--- npyodbc/__init__.py
+++ npyodbc/__init__.py
@@ -4,9 +4,10 @@ from types import ModuleType
 from . import _npyodbc
 from ._npyodbc import *  # noqa: F403
 
-# Since the underlying _npyodbc module has a few configuration options that it
-# reads on itself, and because `pyodbc` exposed these to the user, we need to pass those
-# configuration options along to preserve the API. See
+# The underlying `_npyodbc` module has a few configuration options that it reads on
+# itself, and `pyodbc` exposed these to the user. Since `npyodbc` nests the compiled
+# extension as a submodule (`npyodbc._npyodbc`), we now need to pass along those
+# configuration options to preserve the API. See
 # https://docs.python.org/3/reference/datamodel.html#module.__class__ for the approach
 # used here.
 
